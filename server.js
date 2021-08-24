b73bc3961e570803ae79942d73b461703ef956d0
Jessica Wu <84043367+jessibewu@users.noreply.github.com>
2021-08-23 23:31:58 -0400

    fixed db.json file

--- server.js
+++ server.js
@@ -10,17 +10,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// Get & listen to index.html on page load
+// Get & listen to index.html & notes.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html')); 
   });
 
-// Get notes.html
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 })
 
-// Set up the same Get/Post functions here as they grab from the same route:
+// Set up the same Get/Post functions here since it's from the same route:
 app.route("/api/notes")
     .get(function (req, res) {
         res.json(database);
@@ -30,14 +29,14 @@ app.route("/api/notes")
     .post(function (req, res) {
         let jsonFile = path.join(__dirname, "/db/db.json");
         let newNote = req.body;
-        let highestNoteId = 99;
+        let highestNoteId = 100;
 
         // For loop to loop through the array to find the highest ID.
         for (let i = 0; i < database.length; i++) {
             let individualNote = database[i];
 
             if (individualNote.id > highestNoteId) {
-                // This limits the highestId to be the highest id/number in the array, which is 99.
+                // This sets the highestId to be the highest id/number in the array, which is 100.
                 highestNoteId = individualNote.id;
             }
         }
@@ -45,13 +44,12 @@ app.route("/api/notes")
         newNote.id = highestNoteId + 1;
         database.push(newNote)
 
-        // Add newNote to the db.json file via WriteFile:
+        // Add new notes to the db.json file via WriteFile:
         fs.writeFile(jsonFile, JSON.stringify(database), function (err) {
-
             if (err) {
                 return console.log(err);
             }
-            console.log("Your note was saved!");
+            console.log("Your note has been saved!");
         });
         res.json(newNote);
     });
